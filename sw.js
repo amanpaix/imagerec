1f803720ed7722bd43b9725c9be9301f099834f2
Aman <aman@ratiocinativesolutions.com>
2019-11-18 00:25:42 +0530

    Intia commit

---
+++ sw.js
@@ -0,0 +1,129 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "favicon.ico",
+    "revision": "2cab47d9e04d664d93c8d91aec59e812"
+  },
+  {
+    "url": "index.html",
+    "revision": "ea828ef07fa2b0e03addb863e719e90c"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "736d67820d0fc29f90fe6ad487d57cb3"
+  },
+  {
+    "url": "src/css/app.css",
+    "revision": "dc2e7652d77e3e0ce746641592abc77f"
+  },
+  {
+    "url": "src/css/bootstrap.min.css",
+    "revision": "a15c2ac3234aa8f6064ef9c1f7383c37"
+  },
+  {
+    "url": "src/css/feed.css",
+    "revision": "729d4d24de8f530a402372b7eecb9ec6"
+  },
+  {
+    "url": "src/css/help.css",
+    "revision": "1c6d81b27c9d423bece9869b07a7bd73"
+  },
+  {
+    "url": "src/images/icons/app-icon-144x144.png",
+    "revision": "83011e228238e66949f0aa0f28f128ef"
+  },
+  {
+    "url": "src/images/icons/app-icon-192x192.png",
+    "revision": "f927cb7f94b4104142dd6e65dcb600c1"
+  },
+  {
+    "url": "src/images/icons/app-icon-256x256.png",
+    "revision": "86c18ed2761e15cd082afb9a86f9093d"
+  },
+  {
+    "url": "src/images/icons/app-icon-384x384.png",
+    "revision": "fbb29bd136322381cc69165fd094ac41"
+  },
+  {
+    "url": "src/images/icons/app-icon-48x48.png",
+    "revision": "45eb5bd6e938c31cb371481b4719eb14"
+  },
+  {
+    "url": "src/images/icons/app-icon-512x512.png",
+    "revision": "d42d62ccce4170072b28e4ae03a8d8d6"
+  },
+  {
+    "url": "src/images/icons/app-icon-96x96.png",
+    "revision": "56420472b13ab9ea107f3b6046b0a824"
+  },
+  {
+    "url": "src/images/icons/apple-icon-114x114.png",
+    "revision": "74061872747d33e4e9f202bdefef8f03"
+  },
+  {
+    "url": "src/images/icons/apple-icon-120x120.png",
+    "revision": "abd1cfb1a51ebe8cddbb9ada65cde578"
+  },
+  {
+    "url": "src/images/icons/apple-icon-144x144.png",
+    "revision": "b4b4f7ced5a981dcd18cb2dc9c2b215a"
+  },
+  {
+    "url": "src/images/icons/apple-icon-152x152.png",
+    "revision": "841f96b69f9f74931d925afb3f64a9c2"
+  },
+  {
+    "url": "src/images/icons/apple-icon-180x180.png",
+    "revision": "2e5e6e6f2685236ab6b0c59b0faebab5"
+  },
+  {
+    "url": "src/images/icons/apple-icon-57x57.png",
+    "revision": "cc93af251fd66d09b099e90bfc0427a8"
+  },
+  {
+    "url": "src/images/icons/apple-icon-60x60.png",
+    "revision": "18b745d372987b94d72febb4d7b3fd70"
+  },
+  {
+    "url": "src/images/icons/apple-icon-72x72.png",
+    "revision": "b650bbe358908a2b217a0087011266b5"
+  },
+  {
+    "url": "src/images/icons/apple-icon-76x76.png",
+    "revision": "bf10706510089815f7bacee1f438291c"
+  },
+  {
+    "url": "src/js/app.js",
+    "revision": "980c389e10435d4a937fa78b16f464c9"
+  },
+  {
+    "url": "src/js/bootstrap.min.js",
+    "revision": "e1d98d47689e00f8ecbc5d9f61bdb42e"
+  },
+  {
+    "url": "src/js/feed.js",
+    "revision": "d88ddbb1dd98bf08f0f570d9ce1d7183"
+  },
+  {
+    "url": "src/js/material.min.js",
+    "revision": "713af0c6ce93dbbce2f00bf0a98d0541"
+  },
+  {
+    "url": "sw-base.js",
+    "revision": "f1a771716f9e1155842e7c5a6a775753"
+  }
+]);
---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "**/*.{ico,html,json,css,png,js}"
+  ],
+  "swSrc": "sw-base.js",
+  "swDest": "sw.js",
+  "globIgnores": [
+    "workbox-config.js"
+  ]
+};
